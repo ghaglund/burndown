5df742c06ccd2a876c0fefa3caf449bbd1b33644
ghaglund <ghaglund@upsales.com>
2017-05-09 16:27:07 +0200

    added insensitive argument inputs

--- index.js
+++ index.js
@@ -111,7 +111,8 @@ controller.on('slash_command', function (slashCommand, message) {
                                                 "title_link": "https://docs.google.com/spreadsheets/d/1tHC8uRyAP7mYiK0M7GuhlWpIIuqoG-V73w6Wl3QpTH8/pubchart?oid=1187944909&format=image",
                                                 "text": "This is how we're doing! @burndown-ops",
                                                 "image_url": "https://docs.google.com/spreadsheets/d/1tHC8uRyAP7mYiK0M7GuhlWpIIuqoG-V73w6Wl3QpTH8/pubchart?oid=1187944909&format=image",
-                                                "color": "#262226"
+                                                "color": "#262226",
+                                                "ts": "123"
                                             }
                                         ]
                     });
